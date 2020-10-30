83df251628147e4d8cf39160973c456cc6a5891c
Vachirachat Sawaddiwat na ayuttya <vachirachat@MacBook-Pro-4.local>
2020-10-30 14:56:05 +0700

    finish login

--- lottery/src/page/signin.js
+++ lottery/src/page/signin.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const firstPage = () => {
     return (
-        <div>
-            <h1>This is sign in</h1>
+        <div style={{backgroundColor:"#2E295C",height: "100vh"}}>
+        <div style={{paddingTop:"20%"}}>
+            <input type="text" class="form-control" placeholder="Mnemonic password" style={{width:"50%", margin:"auto"}}/>
+            <div style={{marginTop:'10px', position:'absolute', right:'25%'}}>
+            <button type="button" class="btn btn-warning" style={{marginRight:'10px'}}>Sign up</button>
+            <button type="button" class="btn btn-success">Sign in</button>
+            </div>
+        </div>
         </div>
     )
 }
