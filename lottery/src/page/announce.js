fefb4c1c1e33caf1c097dee1053d00b9f5e0e5bd
Vachirachat Sawaddiwat na ayuttya <vachirachat@MacBook-Pro-4.local>
2020-10-31 21:46:27 +0700

    finish all page css and component but still have to add logic

--- lottery/src/page/announce.js
+++ lottery/src/page/announce.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
-const announce = () => {
+import spinner from '../pic/spinner.jpeg'
+const Announce = () => {
     return (
         <div style={{backgroundColor:"#2E295C",height: "100vh"}}>
-            <h1>This is announce</h1>
+            <div class="card" style={{width:'50%', backgroundColor:'#484281', margin:'auto'}}>
+            <img src={spinner} style={{height:'500px'}}/>
+            <div class="card-body">
+            
+            <h2 style={{color:'#FFFFFF'}}>LOTTERY RESULT</h2>
+            <h2 style={{color:'#FFFFFF'}}>99 99 99</h2>
+            
+            </div>
+            </div>
+            <button type="button" class="btn btn-success" style={{width:"25%", borderColor:'#FFFFFF', marginTop:'20px', marignRight:'10px'}}>Create</button>
+            
         </div>
     )
 }
 
-export default announce;
+export default Announce;
--- lottery/src/page/buyTicket.js
+++ lottery/src/page/buyTicket.js
@@ -1,11 +1,25 @@
-import React from 'react';
-
-const buyTicket = () => {
+import React, { useState } from 'react';
+import lottery from '../pic/lottery.jpg';
+const Buyticket = () => {
+    const [userticket, setUserticket] = useState('vachirachat')
+    const [timeleft, setTimeleft] = useState('10 : 20 : 40 : 40')
+    
     return (
         <div style={{backgroundColor:"#2E295C",height: "100vh"}}>
-            <h1>This is five page</h1>
+            <div class="card" style={{width:'50%', backgroundColor:'#484281', margin:'auto'}}>
+            <div class="card-body">
+            <img src={lottery} />
+            <h2 style={{color:'#FFFFFF', marginTop:'10px'}}>{`${userticket}`}</h2>
+            <h2 style={{color:'#FFFFFF'}}>COUNTING TIME</h2>
+            <h2 style={{color:'#FFFFFF'}}>{`${timeleft}`}</h2>
+            <div style={{ display:'flex'}}>
+            <input type="text" class="form-control" placeholder="TICKET PRICE" style={{width:"70%", margin:"auto" ,marginTop:'20px'}}/>
+            <button type="button" class="btn btn-success" style={{width:"25%", borderColor:'#FFFFFF', marginTop:'20px', marignRight:'10px'}}>Create</button>
+            </div>
+            </div>
+            </div>
         </div>
     )
 }
 
-export default buyTicket;
+export default Buyticket;
